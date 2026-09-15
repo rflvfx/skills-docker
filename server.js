b5ba246811b0d7a297f49ad140058c76db977d35
Rafael <admtwbful@gmail.com>
2026-09-15 14:54:56 -0300

    Ajusta portas e correcoes de codigo

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require('http');
+const PORT = process.env.PORT || 3000;
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Ola do Docker!\n');
-}).listen(3000, () => console.log('Servidor rodando na porta 3000'));
+}).listen(PORT, () => console.log(`Servidor Docker em http://localhost:${PORT}`));
